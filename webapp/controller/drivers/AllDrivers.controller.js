9015a1fe79e1e1ed57a086185012c28ac01d413e
Diana Gonzalez <dianam.gonzalez@seidor.com>
2022-10-04 00:01:22 +0000

    new views

---
+++ webapp/controller/drivers/AllDrivers.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"com/seidor/usa/dispatchers/util/Util",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+], function (Controller, Util) {
+	"use strict";
+	
+    return Controller.extend("com.seidor.usa.dispatchers.controller.AllDriversPage", {
+
+    });
+});
